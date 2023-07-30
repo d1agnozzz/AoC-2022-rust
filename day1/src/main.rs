2f290c7fb825a36d4b0af6d94c7da90f624de382
Alex Mit <klepoz.672@mail.ru>
2023-07-30 04:46:58 +0300

    day1 refactored to be more idiomatic

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,51 +1,26 @@
-use std::fs;
-
-#[derive(Copy, Clone)]
-#[derive(Default)]
-struct Elf {
-    number: usize,
-    calories: usize,
-}
+use itertools::Itertools;
+use std::{cmp, fs};
 
 fn main() {
     let contents = fs::read_to_string("./input.txt").expect("Couldn't read from a file");
-    let consec_nums_iterator = contents.split_terminator("\n\n");
 
-    let mut top_three = [Elf::default(); 3];
+    let answer_p2 = contents
+        .split("\n\n")
+        .map(|e| {
+            e.lines()
+                .map(|l| l.parse::<usize>().expect("Can't parse"))
+                .sum::<usize>()
+        })
+        .map(cmp::Reverse)
+        .k_smallest(3)
+        .enumerate()
+        .map(|(i, x)| {
+            if let 0 = i {
+                println!("Wealthiest: {}", x.0);
+            }
+            x.0
+        })
+        .sum::<usize>();
 
-    let mut elf_number: usize = 0;
-    for lines in consec_nums_iterator {
-        elf_number += 1;
-
-        let elfs_cals = lines
-            .split_terminator('\n')
-            .map(|s| s.parse::<usize>().unwrap_or(0))
-            .sum();
-        println!("Elf {elf_number} carries {elfs_cals}");
-
-        if elfs_cals > top_three[0].calories {
-            top_three[2] = top_three[1];
-            top_three[1] = top_three[0];
-            top_three[0] = Elf {
-                number: elf_number,
-                calories: elfs_cals,
-            };
-        } else if elfs_cals > top_three[1].calories {
-            top_three[2] = top_three[1];
-            top_three[1] = Elf {
-                number: elf_number,
-                calories: elfs_cals,
-            };
-        } else if elfs_cals > top_three[2].calories {
-            top_three[2] = Elf {
-                number: elf_number,
-                calories: elfs_cals,
-            };
-        }
-    }
-
-    for elf in top_three {
-        println!("Elf number {0} carries {1} calories", elf.number, elf.calories);
-    }
-    println!("Top 3 carry {} calories", top_three.iter().map(|e| e.calories).sum::<usize>());
+    println!("Sum of three most wealthiest: {answer_p2}");
 }
